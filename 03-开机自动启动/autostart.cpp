328807b7c21f3320a02a5cfbcdc50a52b72a45aa
luguanxing <651571925@qq.com>
2016-09-22 00:58:47 +0800

    Autostarter

--- 03-开机自动启动/autostart.cpp
+++ 03-开机自动启动/autostart.cpp
@@ -9,13 +9,13 @@ char src[] = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
 char tar[MAX];
 
 int autostart() {
-	::GetModuleFileName(NULL, tar, MAX);
+	::GetModuleFileName(NULL, tar, MAX);	//获取自身路径
 
-	LONG ret1 = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE, src, 0, KEY_ALL_ACCESS, &hkey);
+	LONG ret1 = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE, src, 0, KEY_ALL_ACCESS, &hkey);	//打开注册表
 	if (ret1 != ERROR_SUCCESS)
 		return 1;
 
-	LONG ret2 = ::RegSetValueEx(hkey, "test", 0, REG_SZ, (const unsigned char*)tar, MAX);
+	LONG ret2 = ::RegSetValueEx(hkey, "test", 0, REG_SZ, (const unsigned char*)tar, MAX);	//修改注册表
 	if (ret2 != ERROR_SUCCESS)
 		return 2;
 
@@ -23,8 +23,6 @@ int autostart() {
 }
 
 int main () {
-
 	autostart();
-
 	return 0;
 };
