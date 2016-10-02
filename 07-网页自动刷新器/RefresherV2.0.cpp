6ae4b3acc9984982db636c4d33e00e9e37a1d9c5
luguanxing <651571925@qq.com>
2016-10-02 19:12:00 +0800

    Refresher V2.0

--- 07-网页自动刷新器/RefresherV2.0.cpp
+++ 07-网页自动刷新器/RefresherV2.0.cpp
@@ -6,34 +6,38 @@ HWND hwnd = NULL;
 HWND hwnd2 = NULL;
 HANDLE hthread = NULL;
 
-void refreshchrome() {	//更新chrome网页
-		::SetForegroundWindow(hwnd2);
-		Sleep(5);
-		::SendMessage(hwnd2, WM_KEYDOWN, VK_F5, 0);
-		::SendMessage(hwnd2, WM_KEYUP, VK_F5, 0);
+void refreshchrome() {	//¸üÐÂchromeÍøÒ³
+	::SetForegroundWindow(hwnd2);
+	Sleep(5);
+	::SendMessage(hwnd2, WM_KEYDOWN, VK_F5, 0);
+	::SendMessage(hwnd2, WM_KEYUP, VK_F5, 0);
+}
+
+void checkwindow() {
+	hwnd = NULL;	//ÕÒµ±Ç°¼ÇÊÂ±¾notepad++ÕýÔÚÐ´µÄ´úÂë
+	hwnd = ::FindWindow("Notepad++", NULL);
+	hwnd2 = NULL;
+	hwnd2=FindWindow("Chrome_WidgetWin_1", NULL);
+	hwnd2=GetParent(hwnd2);		//Òª´Ó×Ó´°¿Ú¾ä±úÕÒµ½¸¸´°¿Ú¾ä±ú
 }
 
 int main() {
 	while (true) {
-			hwnd = NULL;	//找当前记事本notepad++正在写的代码
-			hwnd = ::FindWindow("Notepad++", NULL);
-			hwnd2 = NULL;
-			hwnd2=FindWindow("Chrome_WidgetWin_1", NULL);
-			hwnd2=GetParent(hwnd2);	//要从子窗口句柄找到父窗口句柄
-			char textname[250];
-			GetWindowText(hwnd, textname, 250);		
-			if (hwnd == NULL || hwnd2 == NULL || textname[0] != '*') {	//找不到窗口或未更新则忽略
-				Sleep(5);
-				continue; 
-			} else {	//找到后自动保存并更新chrome网页
-				::SetForegroundWindow(hwnd);
-				keybd_event(VK_CONTROL, 0, 0 ,0);
-				keybd_event(83, 0, 0 ,0);
-				keybd_event(83, 0, KEYEVENTF_KEYUP,0);
-				keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP,0);
-				refreshchrome();
-				::SetForegroundWindow(hwnd);
-			}
+		checkwindow();
+		char textname[250];
+		GetWindowText(hwnd, textname, 250);		
+		if (hwnd == NULL || hwnd2 == NULL || textname[0] != '*') {	//ÕÒ²»µ½´°¿Ú»òÎ´¸üÐÂÔòºöÂÔ
+			Sleep(5);
+			continue; 
+		} else {	//ÕÒµ½ºó×Ô¶¯±£´æ²¢¸üÐÂchromeÍøÒ³
+			::SetForegroundWindow(hwnd);
+			keybd_event(VK_CONTROL, 0, 0 ,0);
+			keybd_event(83, 0, 0 ,0);
+			keybd_event(83, 0, KEYEVENTF_KEYUP,0);
+			keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP,0);
+			refreshchrome();
+			::SetForegroundWindow(hwnd);
+		}
 	}
 	return 0;
 }
