ba276523bc16ec9298e0c6386683c03de5f446d5
luguanxing <651571925@qq.com>
2016-10-12 00:25:41 +0800

    DLL注入

--- 09-DLL注入/injecter.cpp
+++ 09-DLL注入/injecter.cpp
@@ -33,7 +33,7 @@ void inject() {
 	::WriteProcessMemory(hprocess, procdlladdr, dllname, size, &writenum);	//向新空间写入要注入的DLL名称
 	loadfuncaddr = ::GetProcAddress(::GetModuleHandle("kernel32.dll"), loadfunc);	//获得LoadLibraryA的地址,在任何进程空间都一样
 	HANDLE hthread = ::CreateRemoteThread(hprocess, NULL, 0, (LPTHREAD_START_ROUTINE)loadfuncaddr, (LPVOID)procdlladdr, 0, NULL);		//新建线程执行LoadLibrary,参数是已在目标进程新空间写入的DLL名称,注意这个函数在64位下无法成功，可用NtCreateThreadEx
-	::WaitForSingleObject(hthread, INFINITE);	//等待线程执行
+	::WaitForSingleObject(hthread, INFINITE);	//可等待线程执行
 	::CloseHandle(hthread);
 	::CloseHandle(hprocess);
 }
