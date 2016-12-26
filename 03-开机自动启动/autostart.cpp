88fad19a308713c28c0905a920aef17ebbdba36b
luguanxing <651571925@qq.com>
2016-12-26 21:53:27 +0800

    开机自动启动

--- 03-开机自动启动/autostart.cpp
+++ 03-开机自动启动/autostart.cpp
@@ -5,17 +5,16 @@ using namespace std;
 #define MAX 500
 
 HKEY hkey = NULL;
-char src[] = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";	//win7可用
+char src[] = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+char tar[MAX];
 
 /*
-64位目录:"HKEY_LOCAL_MACHINE\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
-32位目录:"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run"
-如果你是32位系统这么写没有问题，如果你是64位系统那就写进前一个目录里面去了。
-推荐的做法RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE | KEY_WOW64_64KEY, &hKey);
+	如果你是32位系统会写进32位目录，如果你是64位系统那会写进64位目录。
+	32位目录:"HKEY_LOCAL_MACHINE\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
+	64位目录:"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run"
+	网上推荐的做法RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE | KEY_WOW64_64KEY, &hKey);
 */
 
-
-char tar[MAX];
 
 int autostart() {
 	::GetModuleFileName(NULL, tar, MAX);	//获取自身路径
